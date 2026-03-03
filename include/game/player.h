3a5bbe17f8d682b72fbbe28bbe59f8f334411bba
FLavenNA <77854188+FLavenNA@users.noreply.github.com>
2026-03-04 01:25:49 +0300

    Update map and give player a FOV angle.

--- include/game/player.h
+++ include/game/player.h
@@ -4,8 +4,9 @@
 #include "u_math.h"
 #include "type_defs.h"
 
-#define MOV_SPEED (2.0f / 1000.0f) // In ms so we divide it by 1000
-#define ROT_SPEED (2.0f / 1000.0f) // In ms so we divide it by 1000
+#define MOV_SPEED   (2.0f / 1000.0f) // In ms so we divide it by 1000
+#define ROT_SPEED   (2.0f / 1000.0f) // In ms so we divide it by 1000
+#define FOV         (M_PI / 3.0f)  // 60 degrees
 
 struct player {
     vec2_t position;

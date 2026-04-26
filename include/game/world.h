cdfed9613fccf53752636d267feb3e556528f45a
FLavenNA <77854188+FLavenNA@users.noreply.github.com>
2026-04-26 16:19:56 +0300

    Add new textures for our sprites

--- include/game/world.h
+++ include/game/world.h
@@ -3,13 +3,14 @@
 
 #include "type_defs.h"
 
-#define TEXTURE_WIDTH 64
-#define TEXTURE_HEIGHT 64
+#define TEXTURE_WIDTH   64
+#define TEXTURE_HEIGHT  64
+#define TEXTURE_COUNT   11
 
 struct world {
-    uint32_t textures[8][TEXTURE_WIDTH * TEXTURE_HEIGHT];
+    uint32_t textures[TEXTURE_COUNT][TEXTURE_WIDTH * TEXTURE_HEIGHT];
 };
 
-void init_textures(uint32_t [8][TEXTURE_WIDTH * TEXTURE_HEIGHT]);
+void init_textures(uint32_t [TEXTURE_COUNT][TEXTURE_WIDTH * TEXTURE_HEIGHT]);
 
 #endif //WORLD_H
--- src/game/world.c
+++ src/game/world.c
@@ -2,7 +2,7 @@
 #include "colors.h"
 #include "u_image.h"
 
-void init_textures(uint32_t textures[8][TEXTURE_WIDTH * TEXTURE_HEIGHT]) {
+void init_textures(uint32_t textures[TEXTURE_COUNT][TEXTURE_WIDTH * TEXTURE_HEIGHT]) {
     load_into_texture(textures[0],  "assets/textures/eagle.png");
     load_into_texture(textures[1],  "assets/textures/redbrick.png");
     load_into_texture(textures[2],  "assets/textures/purplestone.png");
@@ -11,4 +11,7 @@ void init_textures(uint32_t textures[8][TEXTURE_WIDTH * TEXTURE_HEIGHT]) {
     load_into_texture(textures[5],  "assets/textures/mossy.png");
     load_into_texture(textures[6],  "assets/textures/wood.png");
     load_into_texture(textures[7],  "assets/textures/colorstone.png");
+    load_into_texture(textures[8],  "assets/textures/barrel.png");
+    load_into_texture(textures[9],  "assets/textures/pillar.png");
+    load_into_texture(textures[10], "assets/textures/greenlight.png");
 }
